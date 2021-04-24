6318a1260cccb816cef05bedfe99cc5e7a5ab0b8
KumarAmitt <amitt2015@outlook.com>
2021-04-25 01:10:12 +0530

    Configure webpack

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,7 @@
-// Global app controller
+import x from './test'
+
+const num = 33;
+console.log(`I import ${x} from another module. Variable num is ${num}`);
+const a = "hello"
+const b = "everyone"
+console.log(`This is a text: ${a} ${b}`)
