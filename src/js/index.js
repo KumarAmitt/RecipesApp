a675d239931dfe10369a86b87fa90867eb6bdc20
KumarAmitt <amitt2015@outlook.com>
2021-04-26 14:05:37 +0530

    Add Recipe model

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import Search from './models/Search'
+import Recipe from "./models/Recipe";
 import * as searchView from './views/searchView';
 import {elements, renderLoader, clearLoader} from "./views/base";
 
@@ -12,6 +13,7 @@ Global state of the app
 
 const state = {};
 
+// SEARCH CONTROLLER
 const controlSearch = async () => {
   // 1. Get query from view
   const query = searchView.getInput() //TODO
@@ -49,6 +51,10 @@ elements.searchResPages.addEventListener('click', e => {
 })
 
 
+// RECIPE CONTROLLER
+const r = new Recipe(47746)
+r.getRecipe()
+console.log(r);
 
 
 
---
+++ src/js/models/Recipe.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+export default class Recipe{
+  constructor(id){
+    this.id = id;
+  }
+
+  async getRecipe(){
+    try{
+      const res = await axios(`https://forkify-api.herokuapp.com/api/get?rId=${this.id}`);
+      this.title = res.data.recipe.title;
+      this.author = res.data.recipe.publisher;
+      this.img = res.data.recipe.image_url;
+      this.ingredients = res.data.recipe.ingredients;
+    }catch(e){
+      console.log(e);
+      alert(e)
+    }
+  }
+
+  calcTime(){
+    const numIng = this.ingredients.length;
+    const periods = Math.ceil(numIng / 3);
+    this.time = periods * 15;
+  }
+
+  calcServings(){
+    this.servings = 4;
+  }
+}
