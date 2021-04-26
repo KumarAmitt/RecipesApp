e68d4ddcfe50e0405f4f31e01453e00e6c94f7eb
KumarAmitt <amitt2015@outlook.com>
2021-04-26 23:11:50 +0530

    Add controller for rendering shopping List

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,7 @@ import Recipe from "./models/Recipe";
 import List from "./models/List";
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
+import * as listView from './views/listView'
 import {elements, renderLoader, clearLoader} from "./views/base";
 
 /**
@@ -104,6 +105,19 @@ const controlRecipe = async () => {
 
 ['hashchange', 'load'].forEach(ev => window.addEventListener(ev, controlRecipe));
 
+// LIST CONTROLLER
+const controlList = () => {
+  // Create a new list IF there is none yet
+  if(!state.list){
+    state.list = new List();
+  }
+
+  // Add each ingredient to the list and UI
+  state.recipe.ingredients.forEach(el => {
+    const item = state.list.addItem(el.count, el.unit, el.ingredient);
+    listView.renderItem(item);
+  })
+}
 
 // Handling recipe button clicks
 
@@ -118,6 +132,8 @@ elements.recipe.addEventListener('click', e => {
     // Increase button is clicked
     state.recipe.updateServings('inc')
     recipeView.updateServingsIngredients(state.recipe);
+  }else if(e.target.matches('.recipe__btn--add, .recipe__btn--add *')) {
+    controlList();
   }
 })
 
